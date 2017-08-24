18f80d9abec496b521196eb9a9a9da565f57243b
srivasanth <srivasanths@outlook.com>
2017-08-24 17:09:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element = document.getElementById ('main-text');
 element.innerHTML = 'hi this is changed by the javascript file';
+
+//move the image
+var img = document.getElementById('img');
+img.onClick = function() {
+    img.style.marginleft = '100px';
+};
