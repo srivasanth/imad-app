21d1c5c3fdfbd7fdf22c56553b4357c4c3318557
srivasanth <srivasanths@outlook.com>
2017-08-24 19:45:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,21 @@
 var button = document.getElementById('counter');
 button.oncllick = function(){
   
+  var request = new XMLHttpRequest();
   
-  
-  //render the variable in the correct span
-  counter = counter +1;
-  var span = document.getElementById('count');
+  request.onreadystatechange = function() {
+      if (request.readystate === XMLHttpRequest.DONE) {
+          if (request.status===200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
   sapn.innerHTML = counter.toString();
    
-    
+          }
+      }
+      
+  }; 
+  
+    request.open('GET', 'http://srivasanth200.imad.hasura-app.io/counter,true');
+    reques.send(null);
+ 
 };
